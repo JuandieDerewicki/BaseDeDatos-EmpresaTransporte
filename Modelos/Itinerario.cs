f06e44790ab0091b411ae37ea3a9e3eaabddc73b
JuandieDerewicki <jdderewicki@gmail.com>
2023-11-01 11:38:03 -0300

    Creacion en la tabla Itinerario

--- Modelos/Itinerario.cs
+++ Modelos/Itinerario.cs
@@ -1,6 +1,18 @@
-﻿namespace ViajePlusBDAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViajePlusBDAPI.Modelos
 {
     public class Itinerario
     {
+        [Key]
+        public int id_itinerario {  get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string ciudad_origen { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string ciudad_destino {  get; set; }
     }
 }
