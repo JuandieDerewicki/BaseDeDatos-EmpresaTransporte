88a934be9b4462d6772084d451d62030523e71c2
JuandieDerewicki <jdderewicki@gmail.com>
2023-11-01 11:44:26 -0300

    Creacion de la tabla Servicio

--- Modelos/Servicio.cs
+++ Modelos/Servicio.cs
@@ -1,6 +1,32 @@
-﻿namespace ViajePlusBDAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViajePlusBDAPI.Modelos
 {
     public class Servicio
     {
+        [Key]
+        public int id_servicio {  get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string fecha_partida { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string fecha_llegada { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string calidad_servicio { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string tipo_servicio { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public int disponibilidad { get; set; }
+
+        // Faltan agregar FKS
     }
 }
