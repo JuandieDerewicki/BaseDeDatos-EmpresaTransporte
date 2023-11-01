9d0073e04a2e2a73970e99bf4338af01d83f3120
JuandieDerewicki <jdderewicki@gmail.com>
2023-11-01 12:08:18 -0300

    Creacion de tabla Itinerario_PuntoIntermedio

--- Modelos/Itinerario_PuntoIntermedio.cs
+++ Modelos/Itinerario_PuntoIntermedio.cs
@@ -1,6 +1,20 @@
-﻿namespace ViajePlusBDAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViajePlusBDAPI.Modelos
 {
     public class Itinerario_PuntoIntermedio
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Itinerario")]
+        public int id_itinerario { get; set; }
+
+        [ForeignKey("PuntoIntermedio")]
+        public int id_puntoIntermedio { get; set; }
+
+        public virtual Itinerario Itinerario { get; set; }
+        public virtual PuntoIntermedio PuntoIntermedio { get; set; }
     }
 }
