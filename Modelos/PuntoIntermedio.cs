594d7ace50adcae6c527a5bbb784ccf102967a53
JuandieDerewicki <jdderewicki@gmail.com>
2023-11-01 11:39:31 -0300

    Creacion de tabla PuntoIntermedio

--- Modelos/PuntoIntermedio.cs
+++ Modelos/PuntoIntermedio.cs
@@ -1,6 +1,15 @@
-﻿namespace ViajePlusBDAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViajePlusBDAPI.Modelos
 {
     public class PuntoIntermedio
     {
+        [Key]
+        public int id_puntoIntermedio {  get; set; }
+
+        [Required]
+        [StringLength(50)]  
+        public string nombre_ciudad {  get; set; }
+
     }
 }
