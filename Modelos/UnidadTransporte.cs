6be7baece0ef8774770261178c2afcae226f34ef
JuandieDerewicki <jdderewicki@gmail.com>
2023-11-01 11:47:15 -0300

    Creacion de tabla UnidadTransporte

--- Modelos/UnidadTransporte.cs
+++ Modelos/UnidadTransporte.cs
@@ -1,7 +1,25 @@
-﻿namespace ViajePlusBDAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViajePlusBDAPI.Modelos
 {
     public class UnidadTransporte
     {
+        [Key]
+        public int id_unidadTransporte {  get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string tipo_unidad {  get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string categoria { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public int asientos { get; set; }
+
+
 
     }
 }
