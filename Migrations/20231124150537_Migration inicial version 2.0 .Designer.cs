840e87b1426cdd34bdcad8089e2142b0cebc5c1b
Maxruizdz <maxruizdz@hotmail.com>
2023-11-24 12:07:45 -0300

    Migracion realizada, creacion de la base de datos

--- Migrations/20231124150537_Migration inicial version 2.0 .Designer.cs
+++ Migrations/20231124150537_Migration inicial version 2.0 .Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ViajePlusBDAPI;
 
@@ -11,9 +12,11 @@ using ViajePlusBDAPI;
 namespace ViajePlusBDAPI.Migrations
 {
     [DbContext(typeof(MiDbContext))]
-    partial class MiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231124150537_Migration inicial version 2.0 ")]
+    partial class Migrationinicialversion20
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
