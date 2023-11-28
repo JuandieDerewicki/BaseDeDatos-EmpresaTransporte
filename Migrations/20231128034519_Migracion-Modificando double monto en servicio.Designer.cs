5a32affdb56c0684c06961711a9a7b32fefdae79
Maxruizdz <maxruizdz@hotmail.com>
2023-11-28 00:46:29 -0300

    Base de datos-Servicios-Migracion-Monto

--- Migrations/20231128034519_Migracion-Modificando double monto en servicio.Designer.cs
+++ Migrations/20231128034519_Migracion-Modificando double monto en servicio.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ViajePlusBDAPI;
 
@@ -11,9 +12,11 @@ using ViajePlusBDAPI;
 namespace ViajePlusBDAPI.Migrations
 {
     [DbContext(typeof(MiDbContext))]
-    partial class MiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231128034519_Migracion-Modificando double monto en servicio")]
+    partial class MigracionModificandodoublemontoenservicio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
